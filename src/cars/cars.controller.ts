e4b3d340b15bd71ab7f47507a502bc300021b129
Darichel <darichel.perez@gmail.com>
2025-11-10 18:25:50 -0300

    feat/validation pipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -9,6 +9,8 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dtos/create-car.dto';
@@ -29,6 +31,7 @@ export class CarsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createCar(@Body() createCarDto: CreateCarDto) {
     return createCarDto;
   }
--- src/cars/dtos/create-car.dto.ts
+++ src/cars/dtos/create-car.dto.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString()
   readonly brand: string;
+
+  @IsString()
   readonly model: string;
 }
