6722815ab8aeba436deb59d73a4cf5acda322694
Darichel Perez <darichel.perez@gmail.com>
2025-11-10 15:33:59 -0300

    feat/services and request methods

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Honda', 'Ford'];
+  
+  constructor(private carsService: CarsService) {}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: number) {
-    return this.cars[id];
+  getCarById(@Param('id') id: string) {
+    return this.carsService.findOneById(+id);
   }
 }
