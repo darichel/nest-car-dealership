870665b7ac9569c2743e355e24e80c45f014d1c6
Darichel Perez <darichel.perez@gmail.com>
2025-11-10 15:10:56 -0300

    feat/get car by id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Ford'];
 
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Ford'];
+    return this.cars;
   }
-  
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    return this.cars[id];
+  }
 }
