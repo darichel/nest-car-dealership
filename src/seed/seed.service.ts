fd9ff8354f4564305f9a1fb91ff9347ce657a99b
Darichel <darichel.perez@gmail.com>
2025-11-11 00:04:37 -0300

    feat/Export services on its modules and import in SEED module

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from 'src/cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
-
+  constructor(
+    private readonly brandsService: BrandsService,
+    private readonly carsService: CarsService,
+  ) {}
 
   populateDB() {
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
     return `This action executed SEED`;
   }
-
 }
